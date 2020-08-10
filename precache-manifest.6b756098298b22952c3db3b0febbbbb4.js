793890f79319653d3f599e661397a0de546c35ef
shatabdi <rautshatabdi239@gmail.com>
2020-08-10 21:02:57 +0530

    Updates

---
+++ precache-manifest.6b756098298b22952c3db3b0febbbbb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ebed5cfb55dd4225445965c436be81",
+    "url": "/abc/index.html"
+  },
+  {
+    "revision": "3657d82abe6dc6e73394",
+    "url": "/abc/static/css/main.a58b0e92.chunk.css"
+  },
+  {
+    "revision": "cc4a6fa0db5e72ba2ecf",
+    "url": "/abc/static/js/2.c10abbee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abc/static/js/2.c10abbee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3657d82abe6dc6e73394",
+    "url": "/abc/static/js/main.71f94623.chunk.js"
+  },
+  {
+    "revision": "dafd85f7ca979c668164",
+    "url": "/abc/static/js/runtime-main.ac3e54e8.js"
+  }
+]);
